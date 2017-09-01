2c8c75eaaf5a328e240c0fb67c3ea1b7d3a85ec2
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-09-01 13:36:24 +0800

    fixed bcrypt

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,11 +1,10 @@
 import * as bcrypt from "bcryptjs";
 
-export const salt = bcrypt.genSaltSync(10);
-
-export const compareHash = (plainText: string) => {
+export const compareHash = (plainText: string, salt: string) => {
   return bcrypt.compareSync(plainText, salt);
 };
 
 export const genHash = (plainText: string): string => {
+  const salt = bcrypt.genSaltSync(10);
   return bcrypt.hashSync(plainText, salt);
 };
