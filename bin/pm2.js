99ff29b422e06425800dce6196f4efc4d8c6d7ed
Kang Kang1 Chen <chenkang1@Lenovo.com>
2017-12-04 13:40:47 +0800

    fixed pm2 script

--- bin/pm2.js
+++ bin/pm2.js
@@ -1,10 +1,13 @@
 const shell = require("shelljs");
 
 shell.exec("pm2 delete all", (code, stdout, stderr) => {
-  stderr && console.log("==========",stderr);
+  try {
+    shell.exec("npm run build");
 
-  shell.exec("cd ./dist && pm2 start index.js -i 4", (code, stdout, stderr) => {
-    stderr && console.log(stderr);
-
-  });
+    shell.exec("cd ./dist && pm2 start index.js -i 4");
+  } catch (error) {
+    console.log("****************************");
+    console.log(error);
+    console.log("****************************");
+  }
 });
