9a5bcd3da233693302b2d26b136c708068d28588
Kang Kang1 Chen <chenkang1@Lenovo.com>
2018-01-29 20:24:09 +0800

    add auto restart node server

---
+++ scripts/shell/restart.node.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "The following node processes were found:"
+ps aux | grep "node " | grep -v grep
+nodepids=$(ps aux | grep "node " | grep -v grep | cut -c10-15)
+
+echo "OK, so we will stop these process/es now..."
+
+for nodepid in ${nodepids[@]};
+do
+echo "Stopping PID :"$nodepid
+kill -9 $nodepid
+done
+echo "Done"
+
+# for nodepid in ${nodepids[@]}
+
+
+
+# echo "kill all nodejs progress ,then start node"
+
+cd ../../ 
+echo "pwd: "`pwd`
+
+npm start
