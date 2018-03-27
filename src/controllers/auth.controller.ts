a53d857f6c9cfa584b05ccb65d6e10ce782b75e8
chenkang084 <chenkang084@163.com>
2018-03-27 17:05:27 +0800

    change soem issues

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,24 +1,42 @@
-import { Request, Response } from "express";
-import BaseController from "./base.controller";
-import { log } from "../utils/common";
-import { BaseResult } from "../pojos/baseResult";
-import config from "../config";
-import * as session from "express-session";
-import { login, logout } from "../services/auth.service";
+import { Request, Response } from 'express';
+import BaseController from './base.controller';
+import { log } from '../utils/common';
+import { BaseResult } from '../pojos/baseResult';
+import config from '../config';
+import * as session from 'express-session';
+import { login, logout } from '../services/auth.service';
 export default class AuthController extends BaseController {
   // sign in the app
   signIn = (req: Request, res: Response) => {
-    const url = req.url.replace(/^\/auth\/+/, "");
-    login.call(this, { url, method: "post", data: req.body }, res);
+    const url = req.url.replace(/^\/auth\/+/, '');
+    const { userName, password } = req.body;
+    const data = {
+      auth: {
+        identity: {
+          methods: ['password'],
+          password: {
+            user: {
+              name: userName,
+              domain: {
+                name: 'Default',
+              },
+              password,
+            },
+          },
+        },
+      },
+    };
+
+    login.call(this, { url, method: 'post', data }, res);
   };
 
   // sign out the app
   signOut = (req: Request, res: Response) => {
-    const url = req.url.replace(/^\/auth\/+/, "");
+    const url = req.url.replace(/^\/auth\/+/, '');
     logout.call(
       this,
-      { url, method: "delete", headers: this.processHeaders() },
-      res
+      { url, method: 'delete', headers: this.processHeaders() },
+      res,
     );
   };
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,19 +1,20 @@
-import { openStackService, OpenStackServiceOpts } from "./axios.service";
-import { Request, Response } from "express";
+import { openStackService, OpenStackServiceOpts } from './axios.service';
+import { Request, Response } from 'express';
 
 export async function login(opts: OpenStackServiceOpts, res: Response) {
   try {
     const result = await openStackService(opts);
 
     this.processHeaders({
-      "X-Auth-Token": result.headers["x-subject-token"],
-      "X-Subject-Token": result.headers["x-subject-token"]
+      'X-Auth-Token': result.headers['x-subject-token'],
+      'X-Subject-Token': result.headers['x-subject-token'],
     });
 
-    res.send({ type: "success", items: result.data });
+    res.send({ type: 'success', items: result.data });
   } catch (error) {
     console.log(error);
-    res.send({ type: "failed", msg: error.message || error });
+    res.status(error.status);
+    res.send({ type: 'failed', msg: error.message || error });
   }
 }
 
@@ -22,15 +23,16 @@ export async function logout(opts: OpenStackServiceOpts, res: Response) {
     const result = await openStackService(opts);
 
     this.processHeaders({
-      "X-Auth-Token": "",
-      "X-Subject-Token": ""
+      'X-Auth-Token': '',
+      'X-Subject-Token': '',
     });
-    res.send({ type: "success" });
+    res.send({ type: 'success' });
   } catch (error) {
     console.log(error);
+    res.status(error.status);
     res.send({
-      type: "failed",
-      msg: error.message || error
+      type: 'failed',
+      msg: error.message || error,
     });
   }
 }
