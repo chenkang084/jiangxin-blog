0b0a332bd80bbd52acd225f54752a725bc953905
chenkang084 <chenkang1@lenovo.com>
2018-05-04 14:41:07 +0800

    add linux service restart auto run script,

--- scripts/shell/dockerEnv.sh
+++ scripts/shell/dockerEnv.sh
@@ -10,7 +10,7 @@ docker run --name jx -d -t -p 9001:9001 -v $path:/app jx
 # docker run --name jx -d -t -p 9001:9001 jiangxin
 
 echo "===========start to build jx images by dockerfile==============="
-docker exec -ti jx /bin/bash /app/scripts/shell/setup.sh
+docker exec -i jx /bin/bash /app/scripts/shell/setup.sh
 
 echo "congratulations,you docker env setup successfully!"
 
